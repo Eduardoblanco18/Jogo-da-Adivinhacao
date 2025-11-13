1e7e70fcc0f43a5f343375691d8ac286aca090c2
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-13 10:15:31 -0300

    adicionando condições de menor e maior

--- Jogo-Da-Adivinhacao.cpp
+++ Jogo-Da-Adivinhacao.cpp
@@ -7,15 +7,20 @@ int main (){
     cout << "************************************" << endl;
 
     int numero_secreto = 90;
-    cout << "O numero secreto eh " << numero_secreto << ". Nao conte para ninguem!" << endl;
 
     int chute;
     cout << "Qual o seu chute?" ;
     cin >> chute;
 
-    cout << "O valor do seu chute eh: " << chute << endl;
+    cout << "O valor do seu chute é: " << chute << endl;
 
     if(chute == numero_secreto){
         cout << "Parabéns! Você acertou o número secreto!" <<endl;
+    }
+    else if(chute > numero_secreto){
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto!" << endl;
     }
 }
