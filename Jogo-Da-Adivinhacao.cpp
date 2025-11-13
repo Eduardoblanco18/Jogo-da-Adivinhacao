261ff1695b90f0957b7e2cdafa391b5a33ff679a
Eduardo Domingues Blanco <eduardodominguesblanco@gmail.com>
2025-11-13 10:32:01 -0300

    adicionando laço

--- Jogo-Da-Adivinhacao.cpp
+++ Jogo-Da-Adivinhacao.cpp
@@ -9,21 +9,25 @@ int main (){
     const int NUMERO_SECRETO = 90;
 
     int chute;
-    cout << "Qual o seu chute?" ;
-    cin >> chute;
+    bool acertou, maior;
 
-    cout << "O valor do seu chute é: " << chute << endl;
+    do{  
+        cout << "Qual o seu chute?" ;
+        cin >> chute;
 
-    bool acertou = chute == NUMERO_SECRETO;
-    bool maior = chute > NUMERO_SECRETO;
+        cout << "O valor do seu chute é: " << chute << endl;
 
-    if(acertou){
-        cout << "Parabéns! Você acertou o número secreto!" <<endl;
-    }
-    else if(maior){
-        cout << "Seu chute foi maior que o número secreto!" << endl;
-    }
-    else {
-        cout << "Seu chute foi menor que o número secreto!" << endl;
-    }
+        acertou = chute == NUMERO_SECRETO;
+        maior = chute > NUMERO_SECRETO;
+
+        if(acertou){
+            cout << "Parabéns! Você acertou o número secreto!" <<endl;
+        }
+        else if(maior){
+            cout << "Seu chute foi maior que o número secreto!" << endl;
+        }
+        else {
+            cout << "Seu chute foi menor que o número secreto!" << endl;
+        }
+    } while(!acertou);
 }
